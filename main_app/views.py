808c33a57cd4fe4e59716e9080a420a6eb97ad8f
Kathy <kath.scholle@gmail.com>
2023-04-15 12:41:31 -0400

    adds Auth-signup, installed pkages

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+# SIGNUP 
+def signup(request):
+    # POST req: save users in db
+    error_message = '' # declare it first to be overwritten
+    if request.method == 'POST':
+        # create a user in memory using UserCreationForm(to process and validate data)
+        form = UserCreationForm(request.POST) # request.POST: the form data
+        # check if form inputs are valid
+        if form.is_valid():
+            # if valid: save new user to db as 'user'
+            user = form.save()
+            # login the new user
+            login(request, user)
+            # redirect user to home pg
+            return redirect('home')
+        # if not valid: generate an err msg
+        else: 
+            error_message = 'Invalid sign up - please try again.'
+
+    # GET req: sign up form
+    form = UserCreationForm() # send an empty form to client
+    return render(request, 'registration/signup.html', {
+        'form': form, # passing form data
+        'error': error_message
+    })
