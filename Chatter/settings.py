cb0ec6a51a096a7b07caeef9a8ebb1fe903fa454
Kathy <kath.scholle@gmail.com>
2023-04-15 14:06:48 -0400

    adds login and nav

--- Chatter/settings.py
+++ Chatter/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_REDIRECT_URL = '/chatter/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path # define URL patterns for Django app
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
 
-}
+]
