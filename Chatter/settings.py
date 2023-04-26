17aa3fb818efb333c12aa6490e1f6d32eeff2f95
Kathy <kath.scholle@gmail.com>
2023-04-25 23:27:04 -0400

    fix: update allowed hosts

--- Chatter/settings.py
+++ Chatter/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['k-chatter.herokuapp.com'] # for set up CORS
 
 
 # Application definition
