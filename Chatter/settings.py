89014f6009f82d04df12c41fca08278cae795292
Kathy <kath.scholle@gmail.com>
2023-04-28 21:59:06 -0400

    deploy

--- Chatter/settings.py
+++ Chatter/settings.py
@@ -29,7 +29,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['chatter-k.herokuapp.com', '127.0.0.1'] # for set up CORS
 
