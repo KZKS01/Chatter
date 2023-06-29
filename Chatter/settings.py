73e1f5cbfb3afab0fb1a5ed4d112e24eeeec0940
Kathy <kath.scholle@gmail.com>
2023-06-28 23:14:18 -0400

    wip: updates database

--- Chatter/settings.py
+++ Chatter/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': 'KZKS01/chatter-k',
-        'NAME': 'postgres',
+        'NAME': 'chatter-db',
         # 'USER': os.environ['USER'],
         'USER': 'postgres',
         'PASSWORD': os.environ['PASSWORD'],
